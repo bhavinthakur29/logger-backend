edf852577a36201475f971650a4a7385cf68891d
bhavinthakur29 <bhavinthakurshan@gmail.com>
2024-11-11 15:41:34 +0000

    update

--- config/db.js
+++ config/db.js
@@ -3,8 +3,14 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
+  const mongoURI = process.env.MONGO_URI; // Get URI from environment variables
+  if (!mongoURI) {
+    console.error("MONGO_URI is not defined in the .env file");
+    return;
+  }
+
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
