a51d274accb5f234030f69bbb91a52cebac1cde0
bhavinthakur29 <bhavinthakurshan@gmail.com>
2024-11-11 15:36:08 +0000

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,55 +2,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-exports.register = async (req, res) => {
-  try {
-    const { username, password, name } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword, name });
-    await user.save();
-    res
-      .status(201)
-      .json({ success: true, message: "User registered successfully" });
-  } catch (error) {
-    res.status(400).json({ success: false, message: "Registration failed" });
-  }
-};
-
-exports.login = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user)
-      return res
-        .status(400)
-        .json({ success: false, message: "Invalid credentials" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res
-        .status(400)
-        .json({ success: false, message: "Invalid credentials" });
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.json({
-      success: true,
-      token,
-      userId: user._id,
-      name: user.name,
-      currency: user.currency,
-    });
-  } catch (error) {
-    res.status(400).json({ success: false, message: "Login failed" });
-  }
-};
-// controllers/authController.js
-
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-
 const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
